e6a7d685206d6faa32fce41878a21c2060628cf8
mytrofanov <mytrofanovmaxym@gmail.com>
2023-10-18 17:55:43 +0300

    feat: readme

--- src/socket.ts
+++ src/socket.ts
@@ -2,9 +2,9 @@ import { io } from 'socket.io-client';
 
 const URL = 'http://localhost:5000';
 export const myNetwork = import.meta.env.VITE_MY_NETWORK || URL;
-export const myBigNetwork = import.meta.env.VITE_MY_NETWORK_BIG || URL;
+// export const myBigNetwork = import.meta.env.VITE_MY_NETWORK_BIG || URL;
 
-const socket = io(myBigNetwork, {
+const socket = io(myNetwork, {
     autoConnect: false,
     withCredentials: true,
 });
